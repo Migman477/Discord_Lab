e5f70743f823fe09f852a075b3594e1483c78784
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-28 08:31:42 -0600

    bot funcionando

--- practica01/procesador_comandos.py
+++ practica01/procesador_comandos.py
@@ -1,6 +1,6 @@
 import datetime
 
-nombre_bot = "Lin"
+
 
 def obtener_saludo(nombre_bot):
     """
@@ -32,15 +32,57 @@ def calcular_uptime(hora_inicio):
 
   
 def mostrar_ayuda():
-    
+    """
+    Muestra una lista de comandos disponibles
+    """
+
+    comandos_disponibles = [
+        "!saludo - Muestra un saludo del bot",
+        "!recordar [Nombre] - El bot recordara el nombre proporcionado",
+        "!uptime - Muestra el tiempo de actividad del agente",
+        "!ayuda - Muestra esta ayuda"
+    ]
+    return "Comandos disponibles:\n" + "\n".join(comandos_disponibles)
+
 
 def iniciar_agente():
+    #Funcion principal para iniciar el agente
+    NOMBRE_BOT = "Lin"
+    PREFIJO = "!"
+    hora_inicio = datetime.datetime.now()
+    
+    print(obtener_saludo(NOMBRE_BOT))
+    print("Escribe '!ayuda' para ver los comandos disponibles.")
+
+    ejecutando = True
+    while ejecutando:
+        entrada =input(f"{NOMBRE_BOT}> : ").strip()
+
+        if not entrada.startswith(PREFIJO):
+            print("Error: Comando debe comenzar con '!'")
+            continue
+
+        partes = entrada[len(PREFIJO):].split(maxsplit=1)
+        comando = partes[0].lower()
+        argumento = partes[1] if len(partes) > 1 else ""
+
+        if comando == "saludo":
+            print(obtener_saludo(NOMBRE_BOT))
+        elif comando == "recordar":
+            print(procesar_comando_recordar(argumento))
+        elif comando == "uptime":
+            print(calcular_uptime(hora_inicio))
+        elif comando == "ayuda":
+            print(mostrar_ayuda())
+        elif comando == "salir":
+            print("¡Hasta luego!")
+            ejecutando = False
+        else:
+            print(f"Comando desconocido: {comando}. Escribe '!ayuda' para ver los comandos disponibles.")
+
+
 
 def main():
-    obtener_saludo(nombre_bot)
-    procesar_comando_recordar("Recuérdame comprar leche")
-    calcular_uptime(datetime.datetime.now())
-    mostrar_ayuda()
     iniciar_agente()
 
 if __name__ == "__main__":
