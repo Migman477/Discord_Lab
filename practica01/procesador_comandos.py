f32be12069fd2153c94b47d71c0d6ef878768879
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-28 07:20:07 -0600

    Crear el procesador de comandos

---
+++ practica01/procesador_comandos.py
@@ -0,0 +1,27 @@
+import datetime
+
+nombre_bot = "Lin"
+
+def obtener_saludo(nombre_bot):
+    return f"Hola, soy {nombre_bot}. ¿En qué puedo ayudarte?"
+
+def procesar_comando_recordar(argumento):
+
+
+def calcular_uptime(hora_inicio):
+
+
+    
+def mostrar_ayuda():
+
+def iniciar_agente():
+
+def main():
+    obtener_saludo(nombre_bot)
+    procesar_comando_recordar("Recuérdame comprar leche")
+    calcular_uptime(datetime.datetime.now())
+    mostrar_ayuda()
+    iniciar_agente()
+
+if __name__ == "__main__":
+    main()
